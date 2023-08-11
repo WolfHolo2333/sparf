da1d5a0add5ce17e1de873bc804ef341835a452e
WolfHolo2333 <52806694+WolfHolo2333@users.noreply.github.com>
2023-08-11 10:50:58 +0800

    Update run_trainval_set0_work2.sh

--- run_trainval_set0_work2.sh
+++ run_trainval_set0_work2.sh
@@ -1,12 +1,12 @@
 export CUDA_VISIBLE_DEVICES=1
 export PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:21
 
-python run_trainval.py joint_pose_nerf_training/dtu sparf_wo_depth_cons_loss --train_sub 3 --scene scan24
-python run_trainval.py joint_pose_nerf_training/dtu sparf_wo_depth_cons_loss --train_sub 3 --scene scan37
-python run_trainval.py joint_pose_nerf_training/dtu sparf_wo_depth_cons_loss --train_sub 3 --scene scan40
-python run_trainval.py joint_pose_nerf_training/dtu sparf_wo_depth_cons_loss --train_sub 3 --scene scan55
-python run_trainval.py joint_pose_nerf_training/dtu sparf_wo_depth_cons_loss --train_sub 3 --scene scan63
-python run_trainval.py joint_pose_nerf_training/dtu sparf_wo_depth_cons_loss --train_sub 3 --scene scan65
-python run_trainval.py joint_pose_nerf_training/dtu sparf_wo_depth_cons_loss --train_sub 3 --scene scan69
-python run_trainval.py joint_pose_nerf_training/dtu sparf_wo_depth_cons_loss --train_sub 3 --scene scan83
-python run_trainval.py joint_pose_nerf_training/dtu sparf_wo_depth_cons_loss --train_sub 3 --scene scan97
+python run_trainval.py joint_pose_nerf_training/dtu sparf --train_sub 3 --scene scan24
+python run_trainval.py joint_pose_nerf_training/dtu sparf --train_sub 3 --scene scan37
+python run_trainval.py joint_pose_nerf_training/dtu sparf --train_sub 3 --scene scan40
+python run_trainval.py joint_pose_nerf_training/dtu sparf --train_sub 3 --scene scan55
+python run_trainval.py joint_pose_nerf_training/dtu sparf --train_sub 3 --scene scan63
+python run_trainval.py joint_pose_nerf_training/dtu sparf --train_sub 3 --scene scan65
+python run_trainval.py joint_pose_nerf_training/dtu sparf --train_sub 3 --scene scan69
+python run_trainval.py joint_pose_nerf_training/dtu sparf --train_sub 3 --scene scan83
+python run_trainval.py joint_pose_nerf_training/dtu sparf --train_sub 3 --scene scan97
