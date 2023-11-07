1c3a20c9c9f99d1c48a45615e8326cda1f2c7f6a
WolfHolo2333 <52806694+WolfHolo2333@users.noreply.github.com>
2023-11-07 22:27:12 +0800

    Update run_trainval_set0_eval1.sh

--- run_trainval_set0_eval1.sh
+++ run_trainval_set0_eval1.sh
@@ -3,3 +3,45 @@ export PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:21
 
 python run_trainval.py joint_pose_nerf_training/dtu sparf --train_sub 3 --scene scan24 --save_pose_only True
 python run_trainval.py joint_pose_nerf_training/dtu sparf --train_sub 3 --scene scan24 --extract_mesh_only True
+
+python run_trainval.py joint_pose_nerf_training/dtu sparf --train_sub 3 --scene scan37 --save_pose_only True
+python run_trainval.py joint_pose_nerf_training/dtu sparf --train_sub 3 --scene scan37 --extract_mesh_only True
+
+python run_trainval.py joint_pose_nerf_training/dtu sparf --train_sub 3 --scene scan40 --save_pose_only True
+python run_trainval.py joint_pose_nerf_training/dtu sparf --train_sub 3 --scene scan40 --extract_mesh_only True
+
+python run_trainval.py joint_pose_nerf_training/dtu sparf --train_sub 3 --scene scan55 --save_pose_only True
+python run_trainval.py joint_pose_nerf_training/dtu sparf --train_sub 3 --scene scan55 --extract_mesh_only True
+
+python run_trainval.py joint_pose_nerf_training/dtu sparf --train_sub 3 --scene scan63 --save_pose_only True
+python run_trainval.py joint_pose_nerf_training/dtu sparf --train_sub 3 --scene scan63 --extract_mesh_only True
+
+python run_trainval.py joint_pose_nerf_training/dtu sparf --train_sub 3 --scene scan65 --save_pose_only True
+python run_trainval.py joint_pose_nerf_training/dtu sparf --train_sub 3 --scene scan65 --extract_mesh_only True
+
+python run_trainval.py joint_pose_nerf_training/dtu sparf --train_sub 3 --scene scan69 --save_pose_only True
+python run_trainval.py joint_pose_nerf_training/dtu sparf --train_sub 3 --scene scan69 --extract_mesh_only True
+
+python run_trainval.py joint_pose_nerf_training/dtu sparf --train_sub 3 --scene scan83 --save_pose_only True
+python run_trainval.py joint_pose_nerf_training/dtu sparf --train_sub 3 --scene scan83 --extract_mesh_only True
+
+python run_trainval.py joint_pose_nerf_training/dtu sparf --train_sub 3 --scene scan97 --save_pose_only True
+python run_trainval.py joint_pose_nerf_training/dtu sparf --train_sub 3 --scene scan97 --extract_mesh_only True
+
+python run_trainval.py joint_pose_nerf_training/dtu sparf --train_sub 3 --scene scan105 --save_pose_only True
+python run_trainval.py joint_pose_nerf_training/dtu sparf --train_sub 3 --scene scan105 --extract_mesh_only True
+
+python run_trainval.py joint_pose_nerf_training/dtu sparf --train_sub 3 --scene scan106 --save_pose_only True
+python run_trainval.py joint_pose_nerf_training/dtu sparf --train_sub 3 --scene scan106 --extract_mesh_only True
+
+python run_trainval.py joint_pose_nerf_training/dtu sparf --train_sub 3 --scene scan110 --save_pose_only True
+python run_trainval.py joint_pose_nerf_training/dtu sparf --train_sub 3 --scene scan110 --extract_mesh_only True
+
+python run_trainval.py joint_pose_nerf_training/dtu sparf --train_sub 3 --scene scan114 --save_pose_only True
+python run_trainval.py joint_pose_nerf_training/dtu sparf --train_sub 3 --scene scan114 --extract_mesh_only True
+
+python run_trainval.py joint_pose_nerf_training/dtu sparf --train_sub 3 --scene scan118 --save_pose_only True
+python run_trainval.py joint_pose_nerf_training/dtu sparf --train_sub 3 --scene scan118 --extract_mesh_only True
+
+python run_trainval.py joint_pose_nerf_training/dtu sparf --train_sub 3 --scene scan122 --save_pose_only True
+python run_trainval.py joint_pose_nerf_training/dtu sparf --train_sub 3 --scene scan122 --extract_mesh_only True
