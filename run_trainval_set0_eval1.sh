d705b22c881b0eaa571f131212ee73a98a12a760
WolfHolo2333 <52806694+WolfHolo2333@users.noreply.github.com>
2023-11-07 22:15:11 +0800

    Update run_trainval_set0_eval1.sh

--- run_trainval_set0_eval1.sh
+++ run_trainval_set0_eval1.sh
@@ -1,11 +1,5 @@
 export CUDA_VISIBLE_DEVICES=9
 export PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:21
 
-python run_trainval.py joint_pose_nerf_training/dtu sparf --train_sub 3 --scene bmvs_bear --save_pose_only True
-python run_trainval.py joint_pose_nerf_training/dtu sparf --train_sub 3 --scene bmvs_clock --save_pose_only True
-python run_trainval.py joint_pose_nerf_training/dtu sparf --train_sub 3 --scene bmvs_dog --save_pose_only True
-python run_trainval.py joint_pose_nerf_training/dtu sparf --train_sub 3 --scene bmvs_durian --save_pose_only True
-python run_trainval.py joint_pose_nerf_training/dtu sparf --train_sub 3 --scene bmvs_jade --save_pose_only True
-python run_trainval.py joint_pose_nerf_training/dtu sparf --train_sub 3 --scene bmvs_man --save_pose_only True
-python run_trainval.py joint_pose_nerf_training/dtu sparf --train_sub 3 --scene bmvs_sculpture --save_pose_only True
-python run_trainval.py joint_pose_nerf_training/dtu sparf --train_sub 3 --scene bmvs_stone --save_pose_only True
+python run_trainval.py joint_pose_nerf_training/dtu sparf --train_sub 3 --scene scan24 --save_pose_only True
+python run_trainval.py joint_pose_nerf_training/dtu sparf --train_sub 3 --scene scan24 --extract_mesh_only True
